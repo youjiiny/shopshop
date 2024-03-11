63ba6f4c1441d229b654cddb487e676ec6949abc
정유진 <jane320@naver.com>
2024-03-12 02:09:51 +0900

    💄 style: navbar 스타일링 (#1)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
-import { FaPencilAlt } from 'react-icons/fa';
+import { FaPencilAlt, FaSlackHash } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <header>
-      <Link to='/'>
-        <FiShoppingBag />
+    <header className='flex justify-between p-2'>
+      <Link to='/' className='flex text-3xl text-primary'>
+        <FaSlackHash />
         <h1>Shopshop</h1>
       </Link>
-      <nav>
+      <nav className='flex items-center gap-4'>
         <Link to='/products'>Products</Link>
         <Link to='/carts'>Carts</Link>
         <Link to='products/new'>
