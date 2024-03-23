8d7b597e479cf3a72887001b184ae93db71f63e4
정유진 <jane320@naver.com>
2024-03-24 01:34:34 +0900

    💄 style: 회원가입 페이지 스타일링 (#4)

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -20,14 +20,18 @@ const SignUp = () => {
   const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
+    <form
+      className='flex flex-col justify-center items-center gap-6 h-screen'
+      onSubmit={onSubmit}
+    >
+      <div className='flex flex-col gap-2 w-96'>
         <label htmlFor='email'>이메일</label>
         <input
+          className='h-12 p-2 border border-neutral-400 rounded'
           type='text'
           id='email'
           {...register('email', {
-            required: '이메일을 입력해주세요',
+            required: '이메일을 입력해주세요.',
             pattern: {
               value: EMAIL_REGEX,
               message: 'example@test.com 형식으로 입력해주세요',
@@ -41,13 +45,14 @@ const SignUp = () => {
           </div>
         )}
       </div>
-      <div>
+      <div className='flex flex-col gap-2 w-96'>
         <label htmlFor='password'>비밀번호</label>
         <input
+          className='h-12 p-2 border border-neutral-400 rounded'
           type='password'
           id='password'
           {...register('password', {
-            required: '비밀번호를 입력해주세요',
+            required: '비밀번호를 입력해주세요.',
             pattern: {
               value: PASSWORD_REGEX,
               message: '영문,숫자 포함 6자 이상 입력해주세요.',
@@ -61,13 +66,14 @@ const SignUp = () => {
           </div>
         )}
       </div>
-      <div>
+      <div className='flex flex-col gap-2 w-96'>
         <label htmlFor='confirm-password'>비밀번호 확인</label>
         <input
+          className='h-12 p-2 border border-neutral-400 rounded'
           type='password'
           id='confirm-password'
           {...register('confirmPassword', {
-            required: '비밀번호를 입력해주세요',
+            required: '비밀번호 확인을 입력해주세요.',
             validate: (confirmPassword) => {
               if (password !== confirmPassword) {
                 return '비밀번호가 일치하지 않습니다.';
@@ -82,7 +88,12 @@ const SignUp = () => {
           </div>
         )}
       </div>
-      <button disabled={!isValid}>회원가입</button>
+      <button
+        className='w-96 h-12 bg-primary rounded disabled:bg-neutral-300 text-white'
+        disabled={!isValid}
+      >
+        회원가입
+      </button>
     </form>
   );
 };
