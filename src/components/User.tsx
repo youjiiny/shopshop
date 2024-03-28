c14129380ca25b1a5a9c13d43103c8d5180f3b7d
정유진 <jane320@naver.com>
2024-03-28 17:36:44 +0900

    🩹 fix: 이미지 없는 경우 기본 이미지로 대체 (#4)

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,7 +4,11 @@ const User = ({ user }: { user: FirebaseUser }) => {
   const { photoURL } = user;
   return (
     <div>
-      <img className='w-9 h-9 rounded-full' src={photoURL!} alt={'프로필'} />
+      <img
+        className='w-9 h-9 rounded-full'
+        src={photoURL ? photoURL : import.meta.env.VITE_PROFILE_DEFAULT_IMG}
+        alt={'프로필'}
+      />
     </div>
   );
 };
