e4b12eb10c471988d22ce3c952605f39b87c928f
정유진 <jane320@naver.com>
2024-05-04 15:43:43 +0900

    ✨ feat: 제품 상세 페이지 정보 가져오기 (#10)

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,13 @@
 import { GetProductType } from 'types/product';
 import { IoMdHeartEmpty } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCard = ({ product }: { product: GetProductType }) => {
-  const { name, image, price } = product;
+  const { id, name, image, price } = product;
+  const navigate = useNavigate();
+
   return (
-    <li className='cursor-pointer'>
+    <li className='cursor-pointer' onClick={() => navigate(`/products/${id}`)}>
       <div>
         <img
           className='w-96 h-96 md:w-72 md:h-72'
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getProudctDetail } from 'api/product';
+import { SelectedProduct } from 'components/SelectedProduct';
+import { useProductCountContext } from 'context/ProductCountContext';
+import { useParams } from 'react-router-dom';
+import { GetProductType, ProductCountContextType } from 'types/product';
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  const { id } = useParams();
+  const {
+    isLoading,
+    error,
+    data: product,
+  } = useQuery<GetProductType>({
+    queryKey: ['products', id],
+    queryFn: () => getProudctDetail(id as string),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  const { name, image, description, price, size } = product!;
+  const {
+    size: option,
+    selected,
+    selectSize,
+    selectProduct,
+  } = (useProductCountContext()! as ProductCountContextType) || {};
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    selectSize(value);
+    selectProduct(value);
+  };
+
+  return (
+    <div className='w-full flex flex-col md:flex-row content-between gap-10 p-10'>
+      <img
+        className='w-full basis-1/2 md:w-140 md:h-140'
+        src={image}
+        alt={'상품 이미지'}
+      />
+      <div className='w-full basis-1/2 flex flex-col gap-2 pl-10'>
+        <h3 className='text-xl font-semibold'>{name}</h3>
+        <p>{`${price.toLocaleString()}원`}</p>
+        <div className='border-b-2'></div>
+        <p>{description}</p>
+        <select
+          className='h-8 border border-gray-400 outline-none cursor-pointer'
+          onChange={handleSelect}
+          defaultValue={''}
+        >
+          <option disabled value={''}>
+            [사이즈]를 선택하세요.
+          </option>
+          {size?.map((s, i) => (
+            <option key={i} value={s}>
+              {s}
+            </option>
+          ))}
+        </select>
+        <button className='h-10 bg-primary text-white'>장바구니 담기</button>
+        {selected && <SelectedProduct option={option} />}
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
