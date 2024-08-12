6f11d1f7402cd5c11de2cc5b929fb3d814310a1a
정유진 <jane320@naver.com>
2024-08-12 23:54:22 +0900

    ✨ feat: 제품 대표 사진 및 상품 사진 추가하도록 설정(#50)

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,5 +1,7 @@
 import { addProduct } from 'api/product';
 import { uploadImage } from 'api/uploader';
+import MainIMageUploader from 'components/MainImageUploader';
+import ProductImageUploader from 'components/ProductImageUploader';
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import { AddProductType } from 'types/product';
@@ -12,27 +14,27 @@ const NewProduct = () => {
     description: '',
     size: '',
   });
-  const [file, setFile] = useState<File | null>(null);
+  const [mainImage, setMainImage] = useState<File | null>();
+  const [subImages, setSubImages] = useState<File[] | null>();
   const [isUploading, setIsUploading] = useState<boolean>(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { files, name, value } = e.target;
-    if (files) {
-      setFile(files![0]);
-      return;
-    }
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
     setProduct({ ...product, [name]: value });
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsUploading(true);
-    const imageUrl = (await uploadImage(file!)) as string;
-    await addProduct(product, imageUrl);
+    //const imageUrl = (await uploadImage(file!)) as string;
+    //await addProduct(product, imageUrl);
     toast.success('제품이 추가되었습니다.');
     setIsUploading(false);
   };
   const isValid = () => {
-    return file &&
+    return mainImage &&
+      //Array.from(subImages!).length > 0 &&
       product.name.trim() !== '' &&
       product.category.trim() !== '' &&
       product.description.trim() !== '' &&
@@ -42,60 +44,70 @@ const NewProduct = () => {
       : false;
   };
   return (
-    <section className='w-full text-center'>
-      <h2 className='text-2xl font-semibold my-4'>제품 등록</h2>
-      {file && (
-        <img
-          className='w-96 mx-auto mb-3'
-          src={URL.createObjectURL(file)}
-          alt={'제품 이미지'}
-        />
-      )}
+    <section className='w-11/12 text-center  mx-auto mt-20 pb-10'>
+      <h2 className='text-2xl font-semibold my-5'>제품 등록</h2>
       <form className='flex flex-col px-12 gap-5' onSubmit={handleSubmit}>
-        <input
-          type='file'
-          accept='image/*'
-          name='img'
-          onChange={handleChange}
-          placeholder='제품 이미지'
-        />
-        <input
-          type='text'
-          name='name'
-          value={product.name}
-          onChange={handleChange}
-          placeholder='제품명'
-        />
-        <input
-          type='number'
-          name='price'
-          value={product.price == 0 ? '' : product.price}
-          onChange={handleChange}
-          placeholder='가격'
-        />
-        <input
-          type='text'
-          name='category'
-          value={product.category}
-          onChange={handleChange}
-          placeholder='카테고리'
-        />
-        <input
-          type='text'
-          name='description'
-          value={product.description}
-          onChange={handleChange}
-          placeholder='제품 설명'
-        />
-        <input
-          type='text'
-          name='size'
-          value={product.size}
-          onChange={handleChange}
-          placeholder='사이즈(콤마(,)로 구분)'
+        <div className='flex items-center'>
+          <h3 className='text-lg'>제품명</h3>
+          <input
+            className='w-1/2 h-10 p-2 ml-8 rounded-md'
+            type='text'
+            name='name'
+            value={product.name}
+            onChange={handleChange}
+            placeholder='제품명'
+          />
+        </div>
+        <div className='flex items-center'>
+          <h3 className='text-lg'>가격</h3>
+          <input
+            className='h-10 ml-12 p-2 rounded-md'
+            type='number'
+            name='price'
+            value={product.price == 0 ? '' : product.price}
+            onChange={handleChange}
+            placeholder='가격'
+          />
+        </div>
+        <div className='flex items-center'>
+          <h3 className='text-lg'>카테고리</h3>
+          <input
+            className='h-10 ml-3 p-2 rounded-md'
+            type='text'
+            name='category'
+            value={product.category}
+            onChange={handleChange}
+            placeholder='카테고리'
+          />
+        </div>
+        <div className='flex items-center'>
+          <h3 className='text-lg'>사이즈</h3>
+          <input
+            className='h-10 ml-7 p-2 rounded-md'
+            type='text'
+            name='size'
+            value={product.size}
+            onChange={handleChange}
+            placeholder='사이즈(콤마(,)로 구분)'
+          />
+        </div>
+        <div className='flex flex-col items-start gap-4'>
+          <h3 className='text-lg'>제품 설명</h3>
+          <textarea
+            className='w-7/12 h-44 border p-2 rounded-md'
+            name='description'
+            value={product.description}
+            onChange={handleChange}
+            placeholder='제품 설명'
+          />
+        </div>
+        <MainIMageUploader mainImage={mainImage!} setMainImage={setMainImage} />
+        <ProductImageUploader
+          subImages={subImages!}
+          setSubImages={setSubImages}
         />
         <button
-          className='h-14 bg-primary rounded disabled:bg-neutral-300 text-2xl text-white'
+          className='h-14 mt-4 bg-primary rounded disabled:bg-neutral-300 text-2xl text-white'
           disabled={!isValid()}
         >
           {isUploading ? '업로드 중...' : '제품 등록하기'}
