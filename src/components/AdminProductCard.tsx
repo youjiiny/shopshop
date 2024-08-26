6506fcfb406c30bb3021d2b0bc3ec0ee670a6fb8
정유진 <jane320@naver.com>
2024-08-26 15:14:28 +0900

    ✨ feat: 등록된 상품 확인 후 삭제하기 (#50)

--- src/components/AdminProductCard.tsx
+++ src/components/AdminProductCard.tsx
@@ -1,22 +1,12 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { deleteProductImg } from 'api/aws';
-import { deleteProduct } from 'api/product';
+import { useModalStore } from 'store/modal';
 import { GetProductType } from 'types/product';
+import ProductDeletionModal from './ProductDeletionModal';
 
 const AdminProductCard = ({ product }: { product: GetProductType }) => {
   const { id, name, image, price, mainImg, subImg } = product;
+  const { openModal } = useModalStore();
   console.log('image', image, 'mainImg', mainImg);
-  const queryClient = useQueryClient();
-  const deleteProductMutate = useMutation({
-    mutationFn: deleteProduct,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['products'] });
-    },
-  });
-  const handleDelete = () => {
-    deleteProductMutate.mutate(id);
-    deleteProductImg(id, mainImg, subImg);
-  };
+
   return (
     <li className='flex items-center justify-around border py-5'>
       <img
@@ -32,9 +22,18 @@ const AdminProductCard = ({ product }: { product: GetProductType }) => {
         <p>{name}</p>
         <p>{`${price.toLocaleString()}원`}</p>
       </div>
-      <div className='flex flex-col'>
-        <button>수정하기</button>
-        <button onClick={handleDelete}>삭제하기</button>
+      <div className='flex flex-col gap-2'>
+        <button className='bg-primary text-white p-2'>수정하기</button>
+        <button
+          className='bg-primary text-white p-2'
+          onClick={() =>
+            openModal(
+              <ProductDeletionModal product={{ id, mainImg, subImg }} />,
+            )
+          }
+        >
+          삭제하기
+        </button>
       </div>
     </li>
   );
