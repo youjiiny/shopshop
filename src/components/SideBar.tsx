3b689b507bb486bc490f4729d450ecf7f33c9408
정유진 <jane320@naver.com>
2024-09-18 16:53:50 +0900

    💄 style: 사이드바 활성 링크에 굵게 표시 (#65)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,51 +1,69 @@
 import { useAuthContext } from 'context/AuthContext';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { AuthContextType } from 'types/auth';
 
 const SideBar = () => {
   const { user } = useAuthContext() as AuthContextType;
-  if (user?.isAdmin) {
-    return (
-      <div className='w-40 lg:w-52'>
-        <div className='mb-10'>
-          <h3 className='text-2xl'>{user?.displayName}</h3>
-        </div>
-        <h4 className='font-bold mb-2'>운영관리</h4>
-        <ul className='text-sm text-light-gray mb-8'>
-          <li>
-            <Link to='/admin/product'>상품관리</Link>
-          </li>
-        </ul>
-        <h4 className='font-bold mb-2'>나의 계정 설정</h4>
-        <ul className='text-sm text-light-gray'>
-          <li>
-            <Link to='/mypage/edit/info'>회원정보 수정</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+  const setActiveLinkStyle = ({ isActive }: { isActive: boolean }) =>
+    isActive ? 'font-bold' : 'font-normal';
 
   return (
     <div className='w-40 lg:w-52'>
       <div className='mb-10'>
         <h3 className='text-2xl'>{user?.displayName}</h3>
       </div>
-      <h4 className='font-bold mb-2'>나의 쇼핑정보</h4>
-      <ul className='text-sm text-light-gray mb-8'>
-        <li>
-          <Link to='/mypage/orders'>주문배송조회</Link>
-        </li>
-        <li>
-          <Link to='/mypage/cancel-orders'>취소/교환/반품 내역</Link>
-        </li>
-      </ul>
-      <h4 className='font-bold mb-2'>나의 계정 설정</h4>
-      <ul className='text-sm text-light-gray'>
-        <li>
-          <Link to='/mypage/edit/info'>회원정보 수정</Link>
-        </li>
-      </ul>
+      {user?.isAdmin ? (
+        <>
+          <h4 className='font-bold mb-2'>운영관리</h4>
+          <ul className='text-sm text-light-gray mb-8'>
+            <li>
+              <NavLink to='/admin/product' className={setActiveLinkStyle}>
+                상품관리
+              </NavLink>
+            </li>
+          </ul>
+          <h4 className='font-bold mb-2'>나의 계정 설정</h4>
+          <ul className='text-sm text-light-gray'>
+            <li>
+              <NavLink to='/mypage/edit/info' className={setActiveLinkStyle}>
+                회원정보 수정
+              </NavLink>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <h4 className='font-bold mb-2'>나의 쇼핑정보</h4>
+          <ul className='text-sm text-light-gray mb-8'>
+            <li className='py-1'>
+              <NavLink to='/mypage' className={setActiveLinkStyle} end>
+                찜한 상품들
+              </NavLink>
+            </li>
+            <li className='py-1'>
+              <NavLink to='/mypage/orders' className={setActiveLinkStyle}>
+                주문배송조회
+              </NavLink>
+            </li>
+            <li className='py-1'>
+              <NavLink
+                to='/mypage/cancel-orders'
+                className={setActiveLinkStyle}
+              >
+                취소/교환/반품 내역
+              </NavLink>
+            </li>
+          </ul>
+          <h4 className='font-bold mb-2'>나의 계정 설정</h4>
+          <ul className='text-sm text-light-gray'>
+            <li>
+              <NavLink to='/mypage/edit/info' className={setActiveLinkStyle}>
+                회원정보 수정
+              </NavLink>
+            </li>
+          </ul>
+        </>
+      )}
     </div>
   );
 };
