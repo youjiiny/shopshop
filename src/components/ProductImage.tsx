c0b23be0ea41e00ead6bef6089848d09a8a7f8cb
정유진 <jane320@naver.com>
2024-10-23 14:00:29 +0900

    ⚡️perf: 상품 디테일 이미지 사이즈 최적화 (#68)

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -2,27 +2,20 @@ type Props = { id: string; subImg: string[] };
 
 const ProductImage = ({ id, subImg }: Props) => {
   return (
-    <>
-      <h2 className='text-2xl font-bold'>상품 정보</h2>
-      <div className='flex flex-col gap-10'>
-        <picture>
-          {subImg?.map((img, i) => (
-            <>
-              <source
-                srcSet={`${import.meta.env.VITE_S3_SHOPSHOP_PRODUCT_URL}/${id}/${img}`}
-                type='image/webp'
-              />
-              <img
-                className='w-full object-cover'
-                src={`${import.meta.env.VITE_S3_SHOPSHOP_PRODUCT_URL}/${id}/${img}`}
-                key={i}
-                alt='상품 디테일 이미지'
-              />
-            </>
-          ))}
-        </picture>
+    <section className='mt-14 px-10'>
+      <h2 className='text-2xl font-bold mb-5'>상품 설명</h2>
+      <div className='flex flex-col'>
+        {subImg?.map((img, i) => (
+          <img
+            key={i}
+            className='object-cover mb-16'
+            src={`${import.meta.env.VITE_S3_SHOPSHOP_PRODUCT_URL}/${id}/${img}`}
+            width={'100%'}
+            alt='상품 디테일 이미지'
+          />
+        ))}
       </div>
-    </>
+    </section>
   );
 };
 
