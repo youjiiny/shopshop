cfb60533f8521f6011dec2ba33c5efd8c156056b
정유진 <jane320@naver.com>
2024-11-24 01:21:40 +0900

    ⚡️ perf: 배너 이미지 최적화 (#68)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,24 @@
 const Banner = () => {
   return (
     <div className='h-96 bg-slate-50'>
-      <div className='w-full h-full bg-cover bg-center bg-banner' />
+      <picture className='w-full h-full'>
+        <source
+          srcSet='/images/banner-large.webp'
+          type='image/webp'
+          media='(min-width: 1200px)'
+          className='w-full h-full object-cover object-center'
+        />
+        <source
+          srcSet='/images/banner-medium.webp'
+          type='image/webp'
+          media='(max-width: 1199px)'
+          className='w-full h-full object-cover object-center'
+        />
+        <img
+          src='/images/banner.jpg'
+          className='w-full h-full object-cover object-center'
+        />
+      </picture>
     </div>
   );
 };
