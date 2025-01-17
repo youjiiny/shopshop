38d5042a84efb84c0b5b7a4afdaa0b90934f98b6
정유진 <jane320@naver.com>
2025-01-17 17:56:43 +0900

    refactor: 좋아요 상품이 없는 경우 컴포넌트로 분리 (#74)

--- src/components/LikedProducts.tsx
+++ src/components/LikedProducts.tsx
@@ -4,6 +4,7 @@ import { useAuthContext } from 'context/AuthContext';
 import { Link } from 'react-router-dom';
 import { AuthContextType } from 'types/auth';
 import { LikedProductType } from 'types/product';
+import EmptyLikedProducts from './EmptyLikedProducts';
 
 const LikedProducts = () => {
   const { user } = useAuthContext() as AuthContextType;
@@ -17,24 +18,9 @@ const LikedProducts = () => {
     queryFn: getLikedProducts,
   });
   if (isLoading) return <p>Loading...</p>;
-  if (!products?.length)
-    return (
-      <div className='px-12 pt-12 pb-48'>
-        <div className='pt-20 text-center  text-4xl border-t-4 md:border-b-2 border-black py-24'>
-          <h3 className='text-lg md:text-3xl'>
-            좋아요를 누른 상품이 없습니다.
-          </h3>
-          <div className='flex justify-center mt-10'>
-            <Link
-              to='/'
-              className='flex flex-1 justify-center items-center max-w-60 md:max-w-[400px] h-14 border border-gray-500 text-lg md:text-2xl font-bold'
-            >
-              CONTINUE SHOPPING
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
+  if (!products?.length) {
+    return <EmptyLikedProducts />;
+  }
 
   return (
     <>
